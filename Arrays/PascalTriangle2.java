585b447d0a798592bc74e0660425346f436d5ea8
ROSHINI PASAPU <147152222+pasapuroshini@users.noreply.github.com>
2024-08-21 20:40:16 +0530

    Create PascalTriangle2.java

---
+++ Arrays/PascalTriangle2.java
@@ -0,0 +1,19 @@
+
+
+
+class PascalTriangle2 {
+    public List<Integer> getRow(int rowIndex) {
+        long ans=1;
+        List<Integer> answer=new ArrayList<>();
+        answer.add((int)ans);
+        for(int i=1;i<=rowIndex;i++)
+        {
+             ans=ans*(rowIndex-i+1);
+             ans=ans/i;
+
+          answer.add((int)ans);
+
+        }
+        return answer;
+    }
+}
