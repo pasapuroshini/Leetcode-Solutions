67789cfca21693598c1832a77ebbb47769745339
ROSHINI PASAPU <147152222+pasapuroshini@users.noreply.github.com>
2024-09-21 12:35:10 +0530

    Create 49.java

---
+++ 49.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> mpp= new HashMap<>();
+        for(String word: strs)
+        {
+            char [] chars= word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord= new String(chars);
+            if( !mpp.containsKey(sortedWord))
+            {
+                mpp.put(sortedWord,new ArrayList<>());
+            }
+            mpp.get(sortedWord).add(word);
+        }
+        return new ArrayList<>(mpp.values());
+    }
+}
